de1e5b5831c8385c7818edeb3917d689d318b7c0
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-19 02:11:59 +0400

    create constant object for our base url and endpoint constants

--- app/src/main/java/com/a7medelnoor/emaarinterviewtask/util/Constants.kt
+++ app/src/main/java/com/a7medelnoor/emaarinterviewtask/util/Constants.kt
@@ -1,5 +1,13 @@
 package com.a7medelnoor.emaarinterviewtask.util
-
+/**
+ ** @author Ahmed Elnoor
+ ** Email: ahdnoor4@gmail.com
+ ** Github: @a7medelnoor
+ ** @since 18/07/2022
+ ** This Assessment for Emaar Android developer interview task
+ ** IT people Gulf
+ ** Don't use this code for commercial propose
+ **/
 object Constants{
     const val BASE_URL ="https://randomuser.me/"
     const val END_POINT ="api/?results=100"
