ecd193b5f6d65d397a8af426364fec40684b0474
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-19 02:11:20 +0400

    create my application class for di propose

---
+++ app/src/main/java/com/a7medelnoor/emaarinterviewtask/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.a7medelnoor.emaarinterviewtask
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+/**
+ ** @author Ahmed Elnoor
+ ** Email: ahdnoor4@gmail.com
+ ** Github: @a7medelnoor
+ ** @since 18/07/2022
+ ** This Assessment for Emaar Android developer interview task
+ ** IT people Gulf
+ ** Don't use this code for commercial propose
+ **/
+@HiltAndroidApp
+class MyApplication : Application()
