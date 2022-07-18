9f6dfa801b8fe8244eb93d299d0f8225b0d702f9
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-19 02:32:33 +0400

    setup our navigation jetpack component with nav controller

--- app/src/main/java/com/a7medelnoor/emaarinterviewtask/MainActivity.kt
+++ app/src/main/java/com/a7medelnoor/emaarinterviewtask/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.a7medelnoor.emaarinterviewtask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import dagger.hilt.android.AndroidEntryPoint
+/**
+ ** @author Ahmed Elnoor
+ ** Email: ahdnoor4@gmail.com
+ ** Github: @a7medelnoor
+ ** @since 18/07/2022
+ ** This Assessment for Emaar Android developer interview task
+ ** IT people Gulf
+ ** Don't use this code for commercial propose
+ **/
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
