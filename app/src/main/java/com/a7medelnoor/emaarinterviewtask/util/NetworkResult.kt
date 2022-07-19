21e9e7df054576ee0ed35e69f4da62b5ddfebd79
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-19 22:12:03 +0400

    add network listener class and network result for our response

---
+++ app/src/main/java/com/a7medelnoor/emaarinterviewtask/util/NetworkResult.kt
@@ -0,0 +1,10 @@
+package com.a7medelnoor.emaarinterviewtask.util
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class SUCCESS<T>(data: T) : NetworkResult<T>(data)
+    class ERROR<T>(message: String?, data: T? = null) : NetworkResult<T>(data, message)
+    class LOADING<T> : NetworkResult<T>()
+}
